8f193fde22ace5cf41ff78c08e0b74bfe16abb86
Leonardo-Corazzini <leocora10@gmail.com>
2025-02-25 11:53:14 +0100

    second snack

--- sript.js
+++ sript.js
@@ -21,3 +21,10 @@ const somma3 = (a, b) => {
 }
 
 console.log(somma3(10, 3))
+
+// 🏆 Snack 2
+// Crea una arrow function che calcola il quadrato di un numero.
+// Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+const quadrato = (num) => num ** 2
+console.log(quadrato(7))
+
